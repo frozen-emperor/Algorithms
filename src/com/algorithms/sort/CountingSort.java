96d133057984e8a1c5349f2a059335419fc05ec0
shailesh <mail.shailesh.singh@gmail.com>
2021-10-15 10:21:37 +0530

    added optimized version of counting sort

--- src/com/algorithms/sort/CountingSort.java
+++ src/com/algorithms/sort/CountingSort.java
@@ -14,6 +14,7 @@ public class CountingSort {
 			countingSort(arr);
 		}
 		countingSortWithZeroAllowed();
+		optimizedCountSortForMinMaxValues();
 	}
 
 	static int getHighestValue(int[] arr) {
@@ -29,25 +30,23 @@ public class CountingSort {
 	static void countingSort(int[] arr) {
 		System.out.print("Unsorted Array : ");
 		PrintUtils.printArrays(arr);
-		
-		//we take highest value from the array suppose 10
-		//the we create array of 10 zeroes
+
+		// we take highest value from the array suppose 10
+		// the we create array of 10 zeroes
 		int highest = getHighestValue(arr);
 		int[] countingArr = new int[highest];
 		int index;
 
 		Arrays.fill(countingArr, 0);
-		//for every value in arr we increment counting array's
-		//that particular index/index -1 
+		// for every value in arr we increment counting array's
+		// that particular index/index -1
 		for (int i = 0; i < arr.length; i++) {
 			index = arr[i] - 1;
 			countingArr[index]++;
-		}
-		//PrintUtils.printArrays(arr);
-		//PrintUtils.printArrays(countingArr);
+		}		
 
-		//then we traverse through counting array and
-		//insert the each index of counting array until 
+		// then we traverse through counting array and
+		// insert the each index of counting array until
 		// counting array[index] becomes zero
 		int arrIndex = 0;
 		for (int i = 0; i < countingArr.length; i++) {
@@ -59,32 +58,58 @@ public class CountingSort {
 		System.out.print("Sorted Array   : ");
 		PrintUtils.printArrays(arr);
 	}
-	
-	//above algo supports only positive integers
-	//but this algo supports 0 as well
+
+	// above algo supports only positive integers
+	// but this algo supports 0 as well
 	static void countingSortWithZeroAllowed() {
-		 int[] arr = { 0, 5, 9, 8, 0, 8, 7, 10, 4, 3,1,2 };
-		 System.out.print("Unsorted Array : ");
-		 PrintUtils.printArrays(arr);
-		 int highest = getHighestValue(arr);
-			int[] countingArr = new int[highest+1];
-			int index;
+		int[] arr = { 0, 5, 9, 8, 0, 8, 7, 10, 4, 3, 1, 2 };
+		System.out.print("Unsorted Array : ");
+		PrintUtils.printArrays(arr);
+		int highest = getHighestValue(arr);
+		int[] countingArr = new int[highest + 1];
+		int index;
 
-			Arrays.fill(countingArr, 0);
-			for (int i = 0; i < arr.length; i++) {
-				index = arr[i];
-				countingArr[index]++;
-			}			
+		Arrays.fill(countingArr, 0);
+		for (int i = 0; i < arr.length; i++) {
+			index = arr[i];
+			countingArr[index]++;
+		}
 
-			int arrIndex = 0;
-			for (int i = 0; i < countingArr.length; i++) {
-				while (countingArr[i] > 0) {
-					arr[arrIndex++] = i;
-					countingArr[i]--;
-				}
+		int arrIndex = 0;
+		for (int i = 0; i < countingArr.length; i++) {
+			while (countingArr[i] > 0) {
+				arr[arrIndex++] = i;
+				countingArr[i]--;
 			}
-			System.out.print("Sorted Array   : ");
-			PrintUtils.printArrays(arr);
+		}
+		System.out.print("Sorted Array   : ");
+		PrintUtils.printArrays(arr);
+	}
+
+	static void optimizedCountSortForMinMaxValues() {
+		// this algo is much effiecient than above 2 since
+		// it also takes min and max as input
+		// so space complexity in decreased
+		int[] arr = { 0, 5, 9, 8, 0, 8, 7, 10, 4, 3, 1, 2 };
+		System.out.print("Unsorted Array : ");
+		PrintUtils.printArrays(arr);
+
+		int min = 0, max = 10;
+		int[] countArr = new int[(max - min) + 1];
+		Arrays.fill(countArr, 0);
+
+		for (int i = 0; i < arr.length; i++) {
+			countArr[arr[i] - min]++;
+		}
+		int j = 0;
+		for (int i = 0; i < countArr.length; i++) {
+			while (countArr[i] > 0) {
+				arr[j++] = i + min;
+				countArr[i]--;
+			}
+		}
+		System.out.print("Sorted Array   : ");
+		PrintUtils.printArrays(arr);
 	}
 
 }
